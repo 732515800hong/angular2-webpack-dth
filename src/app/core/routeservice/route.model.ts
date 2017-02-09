d9d66f3cbc9be700f9d7303deaf0e3303731e980
732515800hong <732515800@qq.com>
2017-02-09 20:42:09 +0800

    remove src

--- src/app/core/routeservice/route.model.ts
+++
@@ -1,3 +0,0 @@
-export class ActiveRouters {
-  static nativeRoutes: Array<string> = [];
-}
--- src/app/main/home/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './home.component';
-export * from './jx_product.component';
-export * from './jx_product.service';
